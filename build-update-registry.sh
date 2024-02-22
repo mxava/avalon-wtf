a73062e15a27bed5d6870cedd0006ed3409978ee
Avalon <git@avalon.wtf>
2024-02-21 17:52:06 -0800

    modified build & update script

--- build-update-registry.sh
+++ build-update-registry.sh
@@ -1,8 +1,10 @@
+### multiarch builds not supported with current setup :(
+### but i would probably try something like this:
 # docker buildx build --platform linux/amd64,linux/arm64 -f ./Dockerfile -t avalon-wtf:latest .
 
 
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 docker build -f ./Dockerfile -t avalon-wtf:latest .
-docker tag avalon-wtf:latest $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/avalon-wtf:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/avalon-wtf:latest
+docker tag avalon-wtf:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/avalon-wtf:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/avalon-wtf:latest
