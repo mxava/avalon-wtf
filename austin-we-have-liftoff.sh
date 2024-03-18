c1d77bd696cb5e162c5d7907226ebf7bf1298aa1
Avalon <77029455+mxava@users.noreply.github.com>
2024-03-18 14:59:48 -0700

    Update austin-we-have-liftoff.sh

--- austin-we-have-liftoff.sh
+++ austin-we-have-liftoff.sh
@@ -2,7 +2,7 @@
 ### these require aws-cli, kubectl, and eksctl
 
 eksctl create cluster --name avalon-wtf2 --region us-west-2 --zones us-west-2a,us-west-2b --nodegroup-name avalon-wtf2 --node-type t2.medium --nodes 1 --nodes-min 1 --nodes-max 2 --managed
-kubectl apply -f service.yaml
+kubectl apply -f manifest.yaml
 # This part deploys ingress services - hosts won't be assigned an address without it.
 kubectl apply -f https://github.com/kubernetes/ingress-nginx/blob/main/deploy/static/provider/aws/deploy.yaml
 kubectl apply -f ingress.yaml
